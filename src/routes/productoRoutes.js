61c9acd7728badba1e5900e772d4136f9a30b203
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2024-07-24 01:26:27 -0300

    se modifico la estructura de las rutas de productos para que solo los usuarios logueados puedan trabajar con las mismas

--- src/routes/productoRoutes.js
+++ src/routes/productoRoutes.js
@@ -2,20 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 const productoController = require('../controllers/productoController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
 
 // Ruta GET para obtener todos los productos
-router.get('/productos', productoController.obtenerProductos);
+router.get('/productos',authMiddleware, productoController.obtenerProductos);
 
 // Ruta GET para obtener un producto por ID
-router.get('/productos/:id', productoController.obtenerProductoPorId);
+router.get('/productos/:id',authMiddleware, productoController.obtenerProductoPorId);
 
 // Ruta POST para crear un nuevo producto
-router.post('/productos', productoController.crearProducto);
+router.post('/productos',authMiddleware, productoController.crearProducto);
 
 // Ruta PATCH para actualizar un producto específico
-router.patch('/productos/:id', productoController.actualizarProducto);
+router.patch('/productos/:id',authMiddleware, productoController.actualizarProducto);
 
 // Ruta DELETE para eliminar un producto específico
-router.delete('/productos/:id', productoController.eliminarProducto);
+router.delete('/productos/:id',authMiddleware, productoController.eliminarProducto);
 
 module.exports = router;
